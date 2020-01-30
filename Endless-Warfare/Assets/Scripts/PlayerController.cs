13743f2fa85c784425d9e557a5d36be4db28f466
Lorenzo Petraroli <lorenzo.petraroli@hotmail.it>
2020-01-30 12:16:26 +0100

    Update PlayerController.cs
    - Added controlMode boolean;
    - Adjusted the code to follow the controlMode trigger.
    - Improved the code to follow GameManager.paused trigger also.

--- Endless-Warfare/Assets/Scripts/PlayerController.cs
+++ Endless-Warfare/Assets/Scripts/PlayerController.cs
@@ -10,7 +10,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Turret turret;
     [SerializeField] private Hull hull;
-
+    //control of the tank mode: true for mouse, false for keyboard
+    [SerializeField] private bool controlMode;
     private Rigidbody m_PlayerRigidbody;
     private float m_MovementInputValue;
     private float m_TurnInputValue;
@@ -35,57 +36,71 @@ public class PlayerController : MonoBehaviour
     {
         m_MovementInputValue = 0f;
         m_TurnInputValue = 0f;
+        controlMode = GameManager.instance.GetMouseControls();
+
     }
 
     private void Update()
     {
-        // get hull movements values
-        m_MovementInputValue = Input.GetAxis("Vertical");
-        m_TurnInputValue = Input.GetAxis("Horizontal");
+        if (GameManager.instance.paused == false)
+        {
+            //the game is not paused
 
-        //turret shots
-        if (GameManager.instance.GetMouseControls() == false)
-        {
-            if (Input.GetKey(KeyCode.Space))
+            // get hull movements input values
+            m_MovementInputValue = Input.GetAxis("Vertical");
+            m_TurnInputValue = Input.GetAxis("Horizontal");
+
+            //get turret shots
+            if (controlMode == true)
             {
-                if (turret.CanShoot())
-                    turret.Shoot();
+                //mouse controls
+                if (Input.GetMouseButton(0))
+                {
+                    if (turret.CanShoot())
+                        turret.Shoot();
+                }
             }
-        }
-        else if (Input.GetMouseButton(0) && GameManager.instance.paused == false)
-        {
-            if (turret.CanShoot())
-                turret.Shoot();
-        }
-        
+            else
+            {
+                //keyboard controls
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    if (turret.CanShoot())
+                        turret.Shoot();
+                }
+            }
+        }      
     }
 
     private void FixedUpdate()
     {
-        //move the hull
-        hull.Move(m_MovementInputValue);
-        hull.Turn(m_TurnInputValue);
-
-        //move the turret
-        if (GameManager.instance.GetMouseControls() == false)
+        if (GameManager.instance.paused == false)
         {
-            //every fixed frame check: if the button to rotate turret is down, if so, keep call the rotateturret function, else stop it
-            if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Comma))
+            //move the hull
+            hull.Move(m_MovementInputValue);
+            hull.Turn(m_TurnInputValue);
+
+            //move the turret
+            if (controlMode == false)
             {
-                
+                // stop any eventual rotation
                 transform.Rotate(Vector3.up, 0f);
-                transform.Rotate(Vector3.up, -turret.rotateSpeed * Time.deltaTime);
+
+                // every fixed frame check if the button to rotate turret is down, if so, keep call the RotateTurret() 
+                if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.Comma))
+                {
+                    transform.Rotate(Vector3.up, -turret.rotateSpeed * Time.deltaTime);
+                }
+                else if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Period))
+                {
+
+                    transform.Rotate(Vector3.up, turret.rotateSpeed * Time.deltaTime);
+                }
             }
-            else if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.Period))
-            {
-                transform.Rotate(Vector3.up, 0f);
-                transform.Rotate(Vector3.up, turret.rotateSpeed * Time.deltaTime);
-            }
-            else
-                transform.Rotate(Vector3.up, 0f);
         }
         else
             turret.FollowCamera();
+        
     }
 
 #endregion
